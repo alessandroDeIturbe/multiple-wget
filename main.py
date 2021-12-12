1e89511f057951bbba8e0540be727378b5b0c362
Alessandro De Iturbe <aledei2250@gmail.com>
2021-12-12 19:38:04 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -5,26 +5,32 @@ import sys
 currentdir = os.getcwd()
 os.chdir(currentdir)
 
-urls = []
+files = {}
 
 while True:
     url = input('Insert the download link (leave empty to start the download): ')
-    
     if url == '':
         break
+    name = ''
+    while name == '':
+        name = input('Insert the name of the file: ')
+        if os.path.exists(name):
+            print('File already exists!')
+            name = ''
+        else:
+            break
+    
+    files[name] = {'url': url, 'name': name}
+print(files)
 
-    urls.append(url)
+print('Start downloading...')
+for key, value in files.items():
     
-print('Start downloading the file/s')
-for u in urls:
-    print('Downloading: %s' % u)
-    file_name = input('Enter the file name (with extension): ')
-    
-    with open(file_name, 'wb') as f:
-        r = requests.get(u, allow_redirects=True, stream=True)
+    with open(value['name'], 'wb') as f:
+        r = requests.get(value['url'], stream=True, allow_redirects=True)
         total_length = r.headers.get('content-length')
 
-        if total_length is None:
+        if total_length is None: # no content length header
             f.write(r.content)
         else:
             dl = 0
@@ -35,3 +41,5 @@ for u in urls:
                 done = int(50 * dl / total_length)
                 sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
                 sys.stdout.flush()
+
+            
